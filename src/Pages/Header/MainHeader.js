d2bbd4b7db5a705a342084f73fd254e64cd8cd7e
unknown <rajebul.alam17@gmail.com>
2022-09-06 15:21:24 +0600

    Header Part Implemented

---
+++ src/Pages/Header/MainHeader.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../Images/logo.png';
+
+const MainHeader = () => {
+
+    const menuItem = <>
+        <li><Link className='text-primary text-xl font-medium' to='/'>Home</Link></li>
+        <li><Link className='text-primary text-xl font-medium' to='/about'>About</Link></li>
+        <li><Link className='text-primary text-xl font-medium' to='/contact'>Contact</Link></li>
+    </>;
+
+    return (
+        <div className='bg-white border-bottom'>
+            <div class="navbar w-full lg:w-11/12 mx-auto">
+                <div class="navbar-start">
+                    <div class="dropdown">
+                        <label tabindex="0" class="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItem}
+                        </ul>
+                    </div>
+                    <Link to='/'> <img src={logo} className='w-[150px] h-[80px]' alt="logo" /> </Link>
+                </div>
+                <div class="navbar-end hidden lg:flex">
+                    <ul class="menu menu-horizontal p-0">
+                        {menuItem}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MainHeader;
---
+++ src/Pages/Header/TopHeader.js
@@ -0,0 +1,34 @@
+import { faWhatsapp } from '@fortawesome/fontawesome-free-brands';
+import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import './Header.css';
+
+const TopHeader = () => {
+    return (
+        <div className='bg-primary border-bottom'>
+            <div className='w-full md:w-11/12 px-4 md:px-6 mx-auto'>
+                <div className='flex justify-between items-center'>
+                    <div className='flex'>
+                        <p className='pseudo pr-6 text-xl py-2 text-secondary'>
+                            <FontAwesomeIcon className='text-secondary text-xl pr-3' icon={faPhoneAlt}></FontAwesomeIcon>
+                            0504188232
+                        </p>
+                        <p className='text-xl pl-1 pr-6 py-2 pseudo-whats text-secondary'>
+                            <FontAwesomeIcon className='text-secondary text-xl pr-3' icon={faWhatsapp}></FontAwesomeIcon>
+                            0504188232
+                        </p>
+                    </div>
+                    <div>
+                        <p className='text-xl py-2 px-4 pseudo-email text-secondary'>
+                            <FontAwesomeIcon className='text-secondary text-xl pr-3' icon={faEnvelope}></FontAwesomeIcon>
+                            Habibrahman891@gmail.com
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TopHeader;
